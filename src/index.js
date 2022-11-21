bde2b236ae74f2f4186babac91196b427f266800
hojkim77 <rlaghwnd4534@gmail.com>
2022-11-21 20:21:14 +0900

    prac

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MapContainer from './mapcontainer';
+import LandingPage from './landingpage';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>
 );
 
